e00d96214a1ecf420357c8d4b2d24a330913addf
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-25 20:45:18 -0300

    main view controller

--- src/main/java/com/metatron/workshopjavafxjdbc/gui/MainController.java
+++ src/main/java/com/metatron/workshopjavafxjdbc/gui/MainController.java
@@ -1,23 +1,44 @@
 package com.metatron.workshopjavafxjdbc.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.ScrollPane;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
-
-    @FXML
-    private ScrollPane scrollPane;
-    @FXML
-    private VBox vbox;
+public class MainController implements Initializable {
 
     @FXML
-    public void initialize() {
-        vbox.prefWidthProperty().bind(scrollPane.widthProperty());
-        vbox.prefHeightProperty().bind(scrollPane.heightProperty());
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+
+    @FXML
+    public void OnMenuItemSellerAction() {
+        System.out.println("OnMenuItemSellerAction");
+    }
+    @FXML
+    public void OnMenuItemDepartmentAction() {
+        System.out.println("OnMenuItemDepartmentAction");
+    }
+    @FXML
+    public void OnMenuItemAboutAction() {
+        System.out.println("OnMenuItemAboutAction");
     }
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
