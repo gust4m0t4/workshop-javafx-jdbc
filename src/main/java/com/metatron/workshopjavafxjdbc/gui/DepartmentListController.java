530501642fbeb958d89b1319bc9bd1393c5345b9
gust4m0t4 <gustavomotabr10@gmail.com>
2024-06-27 19:34:22 -0300

    update Department

--- src/main/java/com/metatron/workshopjavafxjdbc/gui/DepartmentListController.java
+++ src/main/java/com/metatron/workshopjavafxjdbc/gui/DepartmentListController.java
@@ -6,16 +6,14 @@ import com.metatron.workshopjavafxjdbc.gui.util.Alerts;
 import com.metatron.workshopjavafxjdbc.gui.util.Utils;
 import com.metatron.workshopjavafxjdbc.model.entities.Department;
 import com.metatron.workshopjavafxjdbc.model.services.DepartmentService;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -40,6 +38,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @FXML
     private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
 
     @FXML
     private Button btNew;
@@ -75,6 +76,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = service.findAll();
         observableList = FXCollections.observableArrayList(list);
         departmentTableView.setItems(observableList);
+        initEditButtons();
     }
     private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
         try {
@@ -105,4 +107,23 @@ public class DepartmentListController implements Initializable, DataChangeListen
         updateTableView();
 
     }
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/com/metatron/workshopjavafxjdbc/DepartmentForm.fxml",Utils.currentStage(event)));
+            }
+        });
+    }
+
 }
